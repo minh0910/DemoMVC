66aec76f21afc8ce325cd013bc3b9c86c3585667
Vu Le Minh <vuminh.091003@gmail.com>
2026-09-23 22:12:12 +0700

    day 16/9: kết nối CSDL

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using DemoMVC.Models;
+
+namespace DemoMVC.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+// Cấu hình precision cho Price tại đây
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Product>()
+                .Property(p => p.Price)
+                .HasPrecision(18, 2);
+        }
+        
+        public DbSet<Product> Products { get; set; }
+    }
+}
